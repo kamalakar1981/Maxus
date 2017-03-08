dab36c76c43de6a73263159b3d20f3cb3181db22
krishnan <krishnan.l@accionlabs.com>
2017-03-08 16:23:29 +0530

    git ignore update

--- Maxus/gulpfile.js
+++ Maxus/gulpfile.js
@@ -1,48 +1,47 @@
 ﻿/// <binding />
 var ts = require('gulp-typescript');
 var gulp = require('gulp');
-var clean = require('gulp-clean');
-
+var rimraf = require('rimraf');
+var sourcemaps = require('gulp-sourcemaps');
 var destPath = './libs/';
 
 // Delete the dist directory
-gulp.task('clean', function () {
-    return gulp.src(destPath)
-        .pipe(clean());
+gulp.task('clean-libs', function (cb) {
+	rimraf(destPath, cb);
 });
 
-gulp.task("scriptsNStyles", function() {
-    gulp.src([
-            'core-js/client/**',
-            'systemjs/dist/system.src.js',
-            'reflect-metadata/**',
-            'rxjs/**',
-            'zone.js/dist/**',
-            '@angular/**',
-            'jquery/dist/jquery.*js',
-            'bootstrap/dist/js/bootstrap.*js'            
-    ], {
-        cwd: "node_modules/**"
-    })
-        .pipe(gulp.dest("./libs"));
+gulp.task('scriptsNStyles', ['clean-libs'], function () {
+	gulp.src([
+		'core-js/client/**',
+		'systemjs/dist/system.src.js',
+		'reflect-metadata/**',
+		'rxjs/**',
+		'zone.js/dist/**',
+		'@angular/**',
+		'jquery/dist/jquery.*js',
+		'bootstrap/dist/js/bootstrap.*js'
+	], {
+			cwd: "node_modules/**"
+		})
+		.pipe(gulp.dest("./libs"));
 });
 
 var tsProject = ts.createProject('wwwroot/tsconfig.json', {
-    typescript: require('typescript')
+	typescript: require('typescript')
 });
 gulp.task('ts', function (done) {
-    //var tsResult = tsProject.src()
-    var tsResult = gulp.src([
-            "wwwroot/**/*.ts"
-    ])
-        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
-    return tsResult.js.pipe(gulp.dest('./Scripts/app'));
+	var tsResult = gulp.src([
+		"wwwroot/**/*.ts"
+	]).pipe(sourcemaps.init())
+		.pipe(ts(tsProject), undefined, ts.reporter.fullReporter())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./Scripts/app'));
 });
 
 gulp.task('watch', ['watch.ts']);
 
 gulp.task('watch.ts', ['ts'], function () {
-    return gulp.watch('wwwroot/**/*.ts', ['ts']);
+	return gulp.watch('wwwroot/**/*.ts', ['ts']);
 });
 
-gulp.task('default', ['scriptsNStyles', 'watch']);
+gulp.task('default', ['scriptsNStyles', 'ts']);
